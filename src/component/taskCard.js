2df8cc8139f5bc3054d8851a064202571425a7af
madevara24 <adityadevara91@gmail.com>
2021-09-11 14:58:53 +0700

    feature(task card): make edit task feature make and edit task feature by adding edit and update button to the taskc ard as well as a form to edit the task data

--- src/component/taskCard.js
+++ src/component/taskCard.js
@@ -3,6 +3,20 @@ import taskStore from '../store/task';
 import BaseComponent from '../component/base';
 
 class TaskCard extends BaseComponent {
+    state = {
+        isEditing: false,
+        editedTaskContent:"",
+        editedTaskTags:""
+    }
+
+    async componentDidMount() {
+        this.setState({
+            isEditing: false,
+            editedTaskContent: this.props.task.content,
+            editedTaskTags: this.props.task.tags.toString(),
+        });
+      }
+
     deleteTask = async (id) => {
         taskStore.deleteItem(id);
     }
@@ -13,22 +27,68 @@ class TaskCard extends BaseComponent {
         taskStore.editItem(task._id, task);
     }
 
+    editTask = async() => {
+        this.state.isEditing = !this.state.isEditing;
+        this.rerender();
+    }
+
+    setTaskContent = (event) => {
+        this.setState({
+            editedTaskContent: event.target.value,
+        });
+    }
+
+    setTaskTags = (event) => {
+        this.setState({
+            editedTaskTags: event.target.value
+        });
+    }
+
+    updateTask = async (event) => {
+        event.preventDefault();
+        console.log("Update task")
+        let tags = this.state.editedTaskTags.split(",")
+        let task = this.props.task;
+        task.content = this.state.editedTaskContent;
+        task.tags = tags;
+        console.log("Edited task", task)
+        await taskStore.editItem(this.props.task._id, task);
+        this.state.isEditing = !this.state.isEditing;
+        this.rerender();
+    }
+
     render() {
         return (
             <div class="card">
                 <h4>Task Section</h4>
                 <button onClick={() => this.finishTask(this.props.task)}>Finish</button>
                 <button onClick={() => this.deleteTask(this.props.task._id)}>Delete</button>
-                <p key={this.props.task._id}>{this.props.task.content}</p>
-                <p>{this.props.task.finished ? 'Finished' : 'Not finished'}</p>
-                <p>{taskStore.checkIsUploaded(this.props.task) ? 'Uploaded' : 'Not Uploaded'}</p>
-                <ul>
-                    {
-                        this.props.task.tags.map((tag) => (
-                            <li>{tag}</li>
-                        ))
-                    }
-                </ul>
+                {
+                    this.state.isEditing ?
+                    <div>
+                        <button onClick={() => this.editTask()}>Undo Edit</button>
+                        <form onSubmit={this.updateTask}>
+                            <p><label for="task_content">Task Content</label><textarea id="task_content" type='text' value={this.state.editedTaskContent} onChange={this.setTaskContent} /></p>
+                            <p><label for="task_tags">Task Tags (Seperate with comma)</label><input id="task_tags" type='text' value={this.state.editedTaskTags} onChange={this.setTaskTags} /></p>
+                            <p><button>Update</button></p>
+                        </form>
+                    </div>
+                    :
+                    <div>
+                        <button onClick={() => this.editTask()}>Edit</button>
+                        <p key={this.props.task._id}>{this.props.task.content}</p>
+                        <p>{this.props.task.finished ? 'Finished' : 'Not finished'}</p>
+                        <p>{taskStore.checkIsUploaded(this.props.task) ? 'Uploaded' : 'Not Uploaded'}</p>
+                        <p>Created at {this.props.task.createdAt}</p>
+                        <ul>
+                            {
+                                this.props.task.tags.map((tag) => (
+                                    <li>{tag}</li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                }
             </div>
         )
     }
